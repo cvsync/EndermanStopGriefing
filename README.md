6a4ccd9a229d590bd79a08ab1311d8de7bfe664f
MAEKAWA Masahide <maekawa@daemon-systems.org>
2023-07-17 17:47:41 +0900

    Release 1.0.0.  - Enderman Stop-Griefing

---
+++ README.md
@@ -0,0 +1,10 @@
+# Enderman Stop-Griefing
+The Minecraft data pack ``Enderman Stop-Griefing'' , which avoid to grief items by enderman.
+
+## Latest Version
+1.0.0
+
+## For Java Edition (1.20, 1.20.1 and above) users
+1.) Download EndermanStopGriefing.zip
+
+2.) Put it into C:\Users\\\<Username\>\AppData\Roaming\.minecraft\saves\\\<World\>\datapacks
---
+++ build.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+VER_MAJOR=1
+VER_MINOR=0
+UUID1=326ad7a9-8639-407f-b054-ea9d9f60297c
+UUID2=a8d31dc4-bfbd-4a3d-b220-c06effd49654
+
+RESOURCE_PACK_NAME=EndermanStopGriefing
+
+RESOURCE_PACK_FILE_JAVA=${RESOURCE_PACK_NAME}.zip
+
+TMPDIR=./tmp
+
+#
+# for Java Edition
+#
+TEXTURES_SRCDIR=${RESOURCE_PACK_NAME}/data/minecraft/tags/blocks
+TEXTURES_DSTDIR=${TMPDIR}/data/minecraft/tags/blocks
+PACK_FORMAT=15
+rm -f ${RESOURCE_PACK_FILE_JAVA}
+rm -f -r ${TMPDIR}
+mkdir -p ${TMPDIR} ${TMPDIR}/data
+mkdir -p ${TMPDIR}/data/minecraft ${TMPDIR}/data/minecraft/tags ${TMPDIR}/data/minecraft/tags/blocks
+cp ${TEXTURES_SRCDIR}/enderman_holdable.json ${TEXTURES_DSTDIR}/enderman_holdable.json
+cp -R ${RESOURCE_PACK_NAME}/pack.png ${TMPDIR}/
+cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+sed "s/XXXPACKFORMATXXX/${PACK_FORMAT}/g" | \
+sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
+sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXUUID1XXX/${UUID1}/g" | \
+sed "s/XXXUUID2XXX/${UUID2}/g" | \
+cat > ${TMPDIR}/pack.mcmeta
+(cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} *)
+rm -f -r ${TMPDIR}
